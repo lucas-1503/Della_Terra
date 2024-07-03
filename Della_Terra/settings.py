bb559dc6be5c58accc690c36eb047beb51193f42
Lucas Valentin Montezuma <lucasvmontezuma@gmail.com>
2024-07-03 11:15:12 -0300

    implantação dos testes

--- Della_Terra/settings.py
+++ Della_Terra/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "store",
     "product",
     "order",
     "rest_framework",
