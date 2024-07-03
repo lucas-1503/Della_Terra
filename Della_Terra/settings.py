531b9b025897ef1c4bbc24ea242c0516babe0cdc
Lucas Valentin Montezuma <lucasvmontezuma@gmail.com>
2024-07-03 17:55:13 -0300

    paginação

--- Della_Terra/settings.py
+++ Della_Terra/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     "product",
     "order",
     "rest_framework",
-    "django_extensions"
+    "django_extensions",
+    "debug_toolbar"
 ]
 
 MIDDLEWARE = [
@@ -39,6 +40,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
 ]
 
 ROOT_URLCONF = "Della_Terra.urls"
@@ -113,3 +115,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
