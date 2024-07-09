4c022918c1167aa94067a3aaa3e3eb4cd4a51405
Lucas Valentin Montezuma <lucasvmontezuma@gmail.com>
2024-07-09 12:03:12 -0300

    git-actions2

--- Della_Terra/settings.py
+++ Della_Terra/settings.py
@@ -71,10 +71,10 @@ WSGI_APPLICATION = "Della_Terra.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE","django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER":os.environ.get("SQL_USER","user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD","password"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'della_terra',
+        "USER":'lucas',
+        "PASSWORD": 'Lucas55418794*',
         "HOST":os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT","5432")
     }
@@ -140,4 +140,5 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 0.0.0.0").split()
+
