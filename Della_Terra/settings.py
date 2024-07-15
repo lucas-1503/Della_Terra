252e7172e98b07259b2369ba15c0bf6f45605670
Lucas Valentin Montezuma <lucasvmontezuma@gmail.com>
2024-07-14 21:15:53 -0300

    commit final

--- Della_Terra/settings.py
+++ Della_Terra/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,7 +47,7 @@ ROOT_URLCONF = "Della_Terra.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR,'Della_Terra','templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- Della_Terra/urls.py
+++ Della_Terra/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 import debug_toolbar
 from rest_framework.authtoken.views import obtain_auth_token
+from Della_Terra import views
 
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
@@ -9,4 +10,6 @@ urlpatterns = [
     re_path('della_terra/(?P<version>(v1|v2))/', include('order.urls')),
     re_path('della_terra/(?P<version>(v1|v2))/', include('product.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
 ]
